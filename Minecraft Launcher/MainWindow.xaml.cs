f1fb2c25cec97b175cec2fd80a6282d6e53a9f05
Louis <obelouix25@gmail.com>
2015-06-22 09:19:32 +0200

    ne vérifie plus via les variables d'environnement mais directement sur le disque si java est installé

--- Minecraft Launcher/MainWindow.xaml.cs
+++ Minecraft Launcher/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
 using Microsoft.Win32;
 
 namespace Minecraft_Launcher
@@ -21,22 +22,75 @@ namespace Minecraft_Launcher
     /// </summary>
     public partial class MainWindow : Window
     {
+        public void download(string URL, string path)
+        {
+            WebClient wc = new WebClient();
+            wc.DownloadFile(URL, path);
+            
+        }
+
         public MainWindow()
         {
-            string JavaPath = Environment.GetEnvironmentVariable("JAVA_HOME");
-            if (string.IsNullOrEmpty(JavaPath))
+            if (!string.IsNullOrEmpty(GetJavaInstallationPath()))
             {
                 MessageBox.Show("La variable d'environnement JAVA_HOME semble vide ou n'existe pas", "Java non détecté");
                 MessageBox.Show("Java va être télécharger sous la version la plus récente", "Téléchargement de Java");
+
+               /* string UserProfile = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+                string DownloadFolder = UserProfile + "\\Downloads";
+               */
 
             }
             else
             {
                 MessageBox.Show("Java semble installé", "Java");
-
+                MessageBox.Show(GetJavaInstallationPath(), "");
             }
 
             InitializeComponent();
         }
+
+        static string GetJavaInstallationPath()
+        {
+            string environmentPath = Environment.GetEnvironmentVariable("JAVA_HOME");
+            if (!string.IsNullOrEmpty(environmentPath))
+            {
+                return environmentPath;
+            }
+
+            const string JAVA_KEY = "SOFTWARE\\JavaSoft\\Java Runtime Environment\\";
+
+            var localKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry32);
+            using (var rk = localKey.OpenSubKey(JAVA_KEY))
+            {
+                if (rk != null)
+                {
+                    string currentVersion = rk.GetValue("CurrentVersion").ToString();
+                    using (var key = rk.OpenSubKey(currentVersion))
+                    {
+                        return key.GetValue("JavaHome").ToString();
+                    }
+                }
+            }
+
+            localKey = RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, RegistryView.Registry64);
+            using (var rk = localKey.OpenSubKey(JAVA_KEY))
+            {
+                if (rk != null)
+                {
+                    string currentVersion = rk.GetValue("CurrentVersion").ToString();
+                    using (var key = rk.OpenSubKey(currentVersion))
+                    {
+                        return key.GetValue("JavaHome").ToString();
+                    }
+                }
+            }
+
+            return null;
+        }
     }
+
+
 }
+
+
